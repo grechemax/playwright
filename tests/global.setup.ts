6245503b23cc643eb8a2734737bf9bcc86653967
Maks Hrech <grechemax1@gmail.com>
2025-12-19 20:39:42 +0100

    Add comment why global setup is not used along with fixture

--- tests/global.setup.ts
+++ tests/global.setup.ts
@@ -1,3 +1,7 @@
+/* Is a global setup file for Playwright tests to authenticate a user and save the session state.
+ It's not needed when using fixtures that handle authentication per test.
+ See playwright.config.ts for usage context.
+*/
 import { test as setup } from '@playwright/test';
 import path from 'path';
 
