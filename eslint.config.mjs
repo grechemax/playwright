5c3c46accdc22e78903937ac8625d23da2eb259d
Maks Hrech <grechemax1@gmail.com>
2025-12-19 20:38:40 +0100

    Update linter config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,63 +1,62 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 import stylistic from '@stylistic/eslint-plugin';
+import eslint from '@eslint/js';
+import tsEslint from 'typescript-eslint';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-    {
-        languageOptions: { globals: globals.browser }
-    },
-
-    pluginJs.configs.recommended,
-    {
-        plugins: {
-            unicorn: eslintPluginUnicorn,
-            '@stylistic': stylistic
-        },
-        name: 'best practices rules',
-        files: ['**/*.js'],
-        rules: {
-            ...pluginJs.configs.recommended.rules,
-            semi: ['error', 'always'],
-            indent: [
-                'error',
-                4,
-                {
-                    SwitchCase: 1
-                }
-            ],
-            'max-len': [
-                'warn',
-                {
-                    code: 140,
-                    ignoreUrls: true,
-                    ignoreStrings: true,
-                    ignoreTemplateLiterals: true
-                }
-            ],
-            'brace-style': ['warn'],
-            'comma-dangle': ['error', 'never'],
-            'no-multiple-empty-lines': ['error', { max: 2 }],
-            'no-trailing-spaces': ['error', { skipBlankLines: false }],
-            'eol-last': ['error', 'always'],
-            'no-var': ['error'],
-            'prefer-const': 'error',
-            'one-var-declaration-per-line': ['error', 'initializations'],
-            'keyword-spacing': ['error', { before: true, after: true }],
-            'unicorn/filename-case': [
-                'error',
-                {
-                    case: 'kebabCase'
-                }
-            ],
-            'comma-spacing': ['error', { before: false, after: true }],
-            '@stylistic/space-infix-ops': 'error',
-            '@stylistic/quotes': ['warn', 'single'],
-            '@stylistic/space-before-blocks': ['error', 'always'],
-            '@stylistic/arrow-spacing': 'error',
-            '@stylistic/comma-spacing': ['error', { before: false, after: true }],
-            '@stylistic/keyword-spacing': ['error', { before: true, after: true }]
+export default tsEslint.config({
+    files: ['**/*.ts'],
+    languageOptions: {
+        parserOptions: {
+            project: true,
+            tsconfigRootDir: process.cwd()
         }
+    },
+    plugins: {
+        '@stylistic': stylistic,
+        unicorn: eslintPluginUnicorn
+    },
+    extends: [eslint.configs.recommended, ...tsEslint.configs.recommended, ...tsEslint.configs.stylistic],
+    rules: {
+        'no-unused-expressions': 'off',
+        '@typescript-eslint/no-unused-expressions': ['off'],
+        '@typescript-eslint/consistent-indexed-object-style': 'off',
+        '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/explicit-member-accessibility': 'error',
+        '@typescript-eslint/no-shadow': 'error',
+        '@typescript-eslint/require-await': 'error',
+        '@typescript-eslint/explicit-function-return-type': [
+            'error',
+            {
+                allowExpressions: true
+            }
+        ],
+        '@stylistic/indent': ['error', 4],
+        '@stylistic/max-len': [
+            'warn',
+            {
+                code: 140,
+                ignoreUrls: true,
+                ignoreStrings: true,
+                ignoreTemplateLiterals: true
+            }
+        ],
+        '@stylistic/brace-style': ['warn'],
+        '@stylistic/operator-linebreak': ['warn', 'after', { overrides: { '?': 'before', ':': 'before' } }],
+        '@stylistic/quotes': ['warn', 'single'],
+        '@stylistic/semi': ['error', 'always'],
+        '@stylistic/comma-dangle': ['error', 'never'],
+        '@stylistic/no-multiple-empty-lines': ['error', { max: 2 }],
+        '@stylistic/no-trailing-spaces': ['error', { skipBlankLines: false }],
+        '@stylistic/eol-last': ['error', 'always'],
+        '@stylistic/space-infix-ops': 'error',
+        '@stylistic/arrow-spacing': 'error',
+        '@stylistic/comma-spacing': ['error', { before: false, after: true }],
+        '@stylistic/keyword-spacing': ['error', { before: true, after: true }],
+        'unicorn/filename-case': [
+            'error',
+            {
+                case: 'kebabCase'
+            }
+        ]
     }
-];
+});
