a1508d2fe886019cfc4174b519361a760f44ca8d
Maks Hrech <grechemax1@gmail.com>
2025-12-19 20:48:42 +0100

    Fix styling and linter issues

--- src/pages/components/item-card-component.ts
+++ src/pages/components/item-card-component.ts
@@ -1,19 +1,19 @@
 import { Locator, Page } from '@playwright/test';
-import { ItemCardDTO } from 'src/models/ItemCardDTO';
+import { ItemCardDTO } from 'src/models/item-card-dto';
 export class ItemCardComponent {
     private readonly name: Locator;
     private readonly price: Locator;
     private readonly description: Locator;
     private readonly cardLocator: Locator;
 
-    constructor(page: Page, cardLocator: Locator) {
+    public constructor(page: Page, cardLocator: Locator) {
         this.cardLocator = cardLocator;
         this.name = this.cardLocator.locator('.inventory_item_name');
         this.price = this.cardLocator.locator('.inventory_item_price');
         this.description = this.cardLocator.locator('.inventory_item_desc');
     }
 
-    async getComponentData(): Promise<ItemCardDTO> {
+    public async getComponentData(): Promise<ItemCardDTO> {
         return {
             name: (await this.name.textContent()) ?? '',
             price: (await this.price.textContent()) ?? '',
@@ -21,7 +21,7 @@ export class ItemCardComponent {
         };
     }
 
-    async clickToDetails(): Promise<void> {
+    public async clickToDetails(): Promise<void> {
         await this.name.click();
         await this.cardLocator.page().waitForURL(/inventory-item.html/); // Wait for navigation
     }
